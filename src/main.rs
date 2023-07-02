257ca64358cc7242e530253b175cdd5aaba142a7
cdknight <me@ramesh.tv>
2023-07-02 02:09:05 -0400

    Working

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,18 @@ use cairo::{Context, ImageSurface};
 use chrono::NaiveDate;
 use poppler::Document;
 use std::{
+    collections::HashMap,
     env,
     fs::{self, File},
     ops::Deref,
 };
+use strfmt::{strfmt, Format};
 
 mod timesheet_info;
 
 use timesheet_info::Timesheet;
+
+const DATE_FORMAT: &str = "%m/%d/%Y";
 
 use crate::timesheet_info::TimesheetData;
 
@@ -24,10 +28,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
         None => {
             println!("Usage: timesheet_gen [timesheet] [info_json]");
-            return Err(Box::new(std::io::Error::new(
-                std::io::ErrorKind::InvalidInput,
-                "Invalid parameters",
-            )));
+            return Err(Box::new(std::io::Error::new(std::io::ErrorKind::InvalidInput, "Invalid parameters")));
         }
     };
 
@@ -48,8 +49,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                             let (w, h) = first_page.size();
                             println!("Width {} height {}", w, h);
 
-                            let surface =
-                                ImageSurface::create(cairo::Format::ARgb32, w as i32, h as i32)?;
+                            let surface = ImageSurface::create(cairo::Format::ARgb32, w as i32, h as i32)?;
 
                             let context = Context::new(&surface)?;
 
@@ -61,23 +61,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
                             // Draw some sample text
                             context.set_font_size(15.0);
-                            context.select_font_face(
-                                "Liberation Serif",
-                                cairo::FontSlant::Normal,
-                                cairo::FontWeight::Bold,
-                            );
+                            context.select_font_face("Liberation Serif", cairo::FontSlant::Normal, cairo::FontWeight::Bold);
 
                             // Draw positional data onto PDF
                             for pos_entry in ts_info.pos_data {
+                                context.set_font_size(pos_entry.font_size);
                                 context.move_to(pos_entry.pos.0, pos_entry.pos.1);
                                 match pos_entry.data_value {
                                     TimesheetData::Date => {
-                                        context.show_text(
-                                            &chrono::offset::Local::now()
-                                                .date_naive()
-                                                .format("%m/%d/%Y")
-                                                .to_string(),
-                                        )?;
+                                        context.show_text(&chrono::offset::Local::now().date_naive().format(DATE_FORMAT).to_string())?;
                                     }
                                     TimesheetData::Str(s) => {
                                         context.show_text(&s)?;
@@ -85,7 +77,117 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                                 };
                             }
 
-                            println!("Finished drawing rectangle onto pdf");
+                            // Draw entry box
+                            // 1: Calculate size of text line
+
+                            let entry_font_size = {
+                                // We add 1 because the last row contains total information for hours and wage
+                                let entry_num = ts_info.entries.len() as f64 + 1.0;
+                                let box_height = ts_info.entry_pos_data.box_bottom.1 - ts_info.entry_pos_data.box_top.1;
+                                let padding_total_height = entry_num * ts_info.entry_pos_data.row_padding;
+
+                                let text_line_height = (box_height - padding_total_height) / entry_num;
+
+                                // The max font size cannot be used, so we have to make it even smaller to accomodate all the entries in the box
+                                if text_line_height < ts_info.entry_pos_data.max_font_size {
+                                    text_line_height
+                                } else {
+                                    ts_info.entry_pos_data.max_font_size
+                                }
+                            };
+
+                            context.set_font_size(entry_font_size);
+
+                            // 2: Draw each entry onto the pdf
+
+                            let mut row_y = ts_info.entry_pos_data.box_top.1;
+                            let mut max_line_height = 0.0;
+
+                            let mut total_hours = 0.0;
+                            let mut total_amnt = 0.0;
+                            let mut total_map = HashMap::new();
+
+                            for entry in ts_info.entries {
+                                for (k, v) in &ts_info.entry_format {
+                                    let v = v.to_string();
+                                    context.move_to(
+                                        *ts_info.entry_pos_data.entry_starts.get(k).expect("Missing column start for timesheet entry"),
+                                        row_y,
+                                    );
+                                    let mut text = "".to_string();
+
+                                    // Special cases
+                                    if k == "total" {
+                                        let hours =
+                                            entry.get("hours").expect("Expected rate field").as_f64().expect("Invalid hours format");
+                                        let rate = entry.get("rate").expect("Expected rate field").as_f64().expect("Invalid rate format");
+                                        let amnt = hours * rate;
+
+                                        let mut map = HashMap::new();
+                                        map.insert("total".to_string(), amnt);
+
+                                        text = strfmt(&v, &map)?;
+
+                                        total_hours += hours;
+                                        total_amnt += amnt;
+
+                                        // Lazy way to do this
+                                        total_map.insert("hours".to_string(), total_hours);
+                                        total_map.insert("total".to_string(), total_amnt);
+                                    } else {
+                                        // BUG: This breaks all number formatting
+                                        let entry_strings: HashMap<String, String> = entry
+                                            .iter()
+                                            .map(|(k, v)| {
+                                                (
+                                                    k.to_owned(),
+                                                    match v.as_str() {
+                                                        Some(s) => s.to_owned(),
+                                                        None => v
+                                                            .as_f64()
+                                                            .expect("Expected either string or f64 entry, but neither was found")
+                                                            .to_string()
+                                                            .to_owned(),
+                                                    },
+                                                )
+                                            })
+                                            .collect();
+                                        text = v.format(&entry_strings)?;
+                                    }
+
+                                    context.show_text(&text)?;
+                                    let line_height = context.text_extents(&text)?.height();
+                                    if line_height > max_line_height {
+                                        max_line_height = line_height;
+                                    };
+                                }
+                                row_y += max_line_height + ts_info.entry_pos_data.row_padding;
+                            }
+
+                            // Hour/total information in last row (TODO should this be hardcoded?)
+
+                            context.move_to(
+                                *ts_info
+                                    .entry_pos_data
+                                    .entry_starts
+                                    .get("hours")
+                                    .expect("Missing column start for total hours in timesheet entry"),
+                                row_y,
+                            );
+
+                            context.show_text(&ts_info.entry_format.get("hours").unwrap().format(&total_map)?)?;
+
+                            context.move_to(
+                                *ts_info
+                                    .entry_pos_data
+                                    .entry_starts
+                                    .get("total")
+                                    .expect("Missing column start for total amount in timesheet entry"),
+                                row_y,
+                            );
+                            context.show_text(&ts_info.entry_format.get("total").unwrap().format(&total_map)?)?;
+
+                            println!("Finished drawing information onto pdf");
 
                             // PNG currently for debugging purposes
                             let mut output_f = File::create("output.png")?;
@@ -97,18 +199,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                         ))),
                     }
                 }
-                None => Err(Box::new(std::io::Error::new(
-                    std::io::ErrorKind::InvalidInput,
-                    "Could not convert timesheet path to string",
-                ))),
+                None => Err(Box::new(std::io::Error::new(std::io::ErrorKind::InvalidInput, "Could not convert timesheet path to string"))),
             }
         }
         None => {
             println!("Usage: timesheet_gen [timesheet] [info_json]");
-            Err(Box::new(std::io::Error::new(
-                std::io::ErrorKind::InvalidInput,
-                "Invalid parameters",
-            )))
+            Err(Box::new(std::io::Error::new(std::io::ErrorKind::InvalidInput, "Invalid parameters")))
         }
     }
 }
--- src/timesheet_info.rs
+++ src/timesheet_info.rs
@@ -1,13 +1,40 @@
+use std::collections::HashMap;
+
 use chrono::NaiveDate;
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 // We need to insert the date when we generate the timesheet if the option exists
 
+// UNUSED
 #[derive(Serialize, Deserialize)]
 pub struct TimesheetEntries {
     pub date: NaiveDate,
+    pub hours: f32,
     pub description: String,
     pub rate: f32,
 }
+
+// Include column offsets, box top/bottom etc. (TODO make it work for entries that do not go by row, but by column)
+// Include row spacing, as some timesheets may have some kind of row divider
+// We can use the box top/bottom to calculate the font size
+// TODO allow for multiple boxes
+#[derive(Serialize, Deserialize)]
+pub struct TimesheetEntryPositionalData {
+    pub box_top: (f64, f64),
+    pub box_bottom: (f64, f64),
+
+    // Date/description may want to be split for flexibility later
+    pub entry_starts: HashMap<String, f64>,
+
+    // Distance between each row
+    pub row_padding: f64,
+
+    // Maximum font size. If the required font size is less than the max font size, then the font size will be reduced.
+    #[serde(default = "max_font_size")]
+    pub max_font_size: f64,
+}
+
+fn max_font_size() -> f64 { 12.0 }
 
 #[derive(Serialize, Deserialize)]
 #[serde(tag = "type", content = "value")]
@@ -29,5 +56,10 @@ pub struct TimesheetPositionalData {
 #[derive(Serialize, Deserialize)]
 pub struct Timesheet {
     pub pos_data: Vec<TimesheetPositionalData>,
-    pub entries: Vec<TimesheetEntries>,
+    pub entries: Vec<HashMap<String, Value>>,
+
+    // First argument is the parameter to format!, and all resulting are keys in the entries
+    pub entry_format: HashMap<String, String>,
+
+    pub entry_pos_data: TimesheetEntryPositionalData,
 }
